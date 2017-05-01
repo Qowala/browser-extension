d558728dc1e9a0b60deb798b5c4c18dd79cac839
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2017-05-01 14:52:17 +0200

    Add vist stats inspired by Mozilla

--- background-script.js
+++ background-script.js
@@ -11,3 +11,34 @@ function onError(error) {
 
 var querying = browser.tabs.query({});
 querying.then(showUrls, onError);
+
+// Load existent stats with the storage API.
+var gettingStoredStats = browser.storage.local.get("hostNavigationStats");
+gettingStoredStats.then(results => {
+  // Initialize the saved stats if not yet initialized.
+  if (!results.hostNavigationStats) {
+    results = {
+      hostNavigationStats: {}
+    };
+  }
+
+  const {hostNavigationStats} = results;
+
+  // Monitor completed navigation events and update
+  // stats accordingly.
+  browser.webNavigation.onCompleted.addListener(evt => {
+    // Filter out any sub-frame related navigation event
+    if (evt.frameId !== 0) {
+      return;
+    }
+
+    const url = new URL(evt.url);
+
+    hostNavigationStats[url.hostname] = hostNavigationStats[url.hostname] || 0;
+    hostNavigationStats[url.hostname]++;
+
+    // Persist the updated stats.
+    browser.storage.local.set(results);
+  }, {
+    url: [{schemes: ["http", "https"]}]});
+});
