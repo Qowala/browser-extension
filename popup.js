e48d01b19d70b056bd817476531fa95419c88d04
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2017-05-03 23:29:50 +0200

    Display list of dates and allow to choose

--- popup.js
+++ popup.js
@@ -19,8 +19,11 @@ gettingStoredStats.then(results => {
   }
 
   const {hostNavigationStats} = results;
-  const today = new Date(new Date().setHours(0, 0, 0, 0));
-  const sortedHostnames = Object.keys(hostNavigationStats[today]).sort((a, b) => {
+  const dates = {
+    'Today': new Date(new Date().setHours(0, 0, 0, 0)),
+    'Yesterday': new Date(new Date(new Date().setHours(0, 0, 0, 0)).getDate() - 1),
+  };
+  const sortedHostnames = Object.keys(hostNavigationStats[dates['Today']]).sort((a, b) => {
     return hostNavigationStats[a] <= hostNavigationStats[b];
   });
 
@@ -32,12 +35,21 @@ gettingStoredStats.then(results => {
   let timeSpentEl = document.querySelector(".time-spent");
   let networkEl = document.querySelector(".network");
   let dateEl = document.querySelector(".date");
+  var displayDate = 'Today';
+  var displayHostname = '';
 
   // Setup listener for dropdown
-  networkEl.onchange=updateDisplayedNetwork;
+  networkEl.onchange=updateDisplayedHostname;
+  dateEl.onchange=updateDisplayedDate;
 
-  function estimateTime(hostname) {
-    const elapsedTime = hostNavigationStats[today][hostname];
+  function estimateTime() {
+    if (typeof hostNavigationStats[dates[displayDate]] == "undefined" ||
+        typeof hostNavigationStats[dates[displayDate]][displayHostname] == "undefined") {
+      timeSpentEl.textContent = `No data yet`;
+      return;
+    }
+
+    const elapsedTime = hostNavigationStats[dates[displayDate]][displayHostname];
 
     if ((elapsedTime / 60 / 60) >= 1) {
       var displayTime = Math.floor(elapsedTime / 60 / 60);
@@ -55,31 +67,60 @@ gettingStoredStats.then(results => {
     timeSpentEl.textContent = `${displayTime} ${displayUnit}`;
   }
 
-  function updateDisplayedNetwork(event) {
+  function updateDisplayedHostname(event) {
     // You can use “this” to refer to the selected element.
     if(!event.target.value) alert('Please Select One');
-    else estimateTime(event.target.value);
+    else {
+      displayHostname = event.target.value;
+      estimateTime();
+    }
+  }
+
+  function updateDisplayedDate(event) {
+    // You can use “this” to refer to the selected element.
+    if(!event.target.value) alert('Please Select One');
+    else {
+      displayDate = event.target.value;
+      estimateTime();
+    }
   }
 
   while(networkEl.firstChild)
     networkEl.removeChild(networkEl.firstChild);
 
-  const MAX_ITEMS = 5;
-  for (let i=0; i < sortedHostnames.length; i++) {
-    if (i >= MAX_ITEMS) {
-      break;
+  // Get hostname of current tab
+  getCurrentTab().then(function(currentHostname) {
+
+    // Display five most visited websites
+    const MAX_ITEMS = 5;
+    for (let i=0; i < sortedHostnames.length; i++) {
+      if (i >= MAX_ITEMS) {
+        break;
+      }
+
+      const networkItem = document.createElement("option");
+      const hostname = sortedHostnames[i];
+
+      networkItem.textContent = `${hostname}`;
+      networkItem.value = `${hostname}`;
+      // Select by default the current website
+      if (sortedHostnames[i] === currentHostname) {
+        networkItem.selected = 'selected';
+      }
+      networkEl.appendChild(networkItem);
     }
-
-    const networkItem = document.createElement("option");
-    const hostname = sortedHostnames[i];
-
-    networkItem.textContent = `${hostname}`;
-    networkEl.appendChild(networkItem);
-    dateEl.textContent = `Today`;
-  }
-
-  // Display time for current tab
-  getCurrentTab().then(function(hostname) {
-    estimateTime(hostname);
+    // Display the spent time for current tab
+    displayHostname = currentHostname;
+    estimateTime();
   });
+
+  const datesList = Object.keys(dates);
+  for (let i=0; i < datesList.length; i++) {
+    const dateItem = document.createElement("option");
+    const date = datesList[i];
+
+    dateItem.textContent = `${date}`;
+    dateItem.value = `${date}`;
+    dateEl.appendChild(dateItem);
+  }
 });
