6a39c12cef5645565c40395acf5398f3ad5b7ae7
Bat <baptiste@gelez.xyz>
2017-05-24 17:58:29 +0100

    Setup ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "standard",
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+    env: {
+      browser: true
+    },
+    globals: {
+      chrome: false
+    }
+};
