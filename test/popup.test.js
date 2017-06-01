052b2d338014cee0f4ccd5340f8082123fbb8df6
Bat <baptiste@gelez.xyz>
2017-06-01 21:30:30 +0100

    Prepare future tests with fake data

--- test/popup.test.js
+++ test/popup.test.js
@@ -7,26 +7,43 @@ const path = require('path')
 // Mock the DOM to allow functions accesssing it to work
 require('jsdom-global')(fs.readFileSync(path.join(__dirname, '../lib/popup.html')))
 
-describe('Popup', () => {
-  before(() => {
+const dates = {
+  'Today': new Date(new Date().setHours(0, 0, 0, 0)),
+  'Yesterday': new Date(new Date(new Date().setDate(new Date().getDate() - 1)).setHours(0, 0, 0, 0))
+}
+const storage = {
+  config: {
+    blacklist: [
+      'twitter.com'
+    ]
+  },
+  hostNavigationStats: {}
+}
+storage.hostNavigationStats[dates.Today] = {
+  'twitter.com': 10
+}
+
+describe('Popup', function () {
+  before(function () {
     // Mock chrome with sinon.chrome in order to simulate the browser API
     global.chrome = chrome
     // Load the code to test
     global.Vue = require('vue')
+    chrome.storage.local.set(storage)
     this.popup = require('../lib/popup.js')
   })
 
-  describe('formatTime', () => {
-    it('should return time in hour when the time spent is over an hour', () => {
+  describe('formatTime', function () {
+    it('should return time in hour when the time spent is over an hour', function () {
       assert.equal(this.popup.formatTime(3650), '1 hour')
     })
-    it('should return time in minute when the time spent is over a minute', () => {
+    it('should return time in minute when the time spent is over a minute', function () {
       assert.equal(this.popup.formatTime(65), '1 minute')
     })
-    it('should return time in second when the time spent is in seconds', () => {
+    it('should return time in second when the time spent is in seconds', function () {
       assert.equal(this.popup.formatTime(1), '1 second')
     })
-    it('should return time with plural when the time spent has several units', () => {
+    it('should return time with plural when the time spent has several units', function () {
       assert.equal(this.popup.formatTime(5), '5 seconds')
     })
   })
