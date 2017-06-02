a8bca77bb225526f17d5f57b010f63caf11247da
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2017-06-02 21:25:55 +0200

    Add tolerance on www. websites

--- test/background-script.test.js
+++ test/background-script.test.js
@@ -9,13 +9,11 @@ const dates = {
 const storage = {
   config: {
     blacklist: [
-      'twitter.com'
+      'twitter.com',
+      'www.api.twitter.com'
     ]
   },
   hostNavigationStats: {}
-}
-storage.hostNavigationStats[dates.Today] = {
-  'twitter.com': 10
 }
 
 describe('Background script', function () {
@@ -41,11 +39,25 @@ describe('Background script', function () {
       const twitterTracking = trackingResult.hostNavigationStats[dates.Today]['twitter.com']
       assert.equal(twitterTracking, 11)
     })
-    it('should increment time on current website with www subdomain', function () {
+    it('should track website with www subdomain even when not specified', function () {
       const tabs = [{url: 'https://www.twitter.com/test'}]
       const trackingResult = this.bgScript.trackWebsite(storage, tabs)
       const twitterTracking = trackingResult.hostNavigationStats[dates.Today]['twitter.com']
       assert.equal(twitterTracking, 11)
+    })
+    it('should track website with special www subdomain as is', function () {
+      const tabs = [{url: 'https://www.api.twitter.com/test'}]
+      const trackingResult = this.bgScript.trackWebsite(storage, tabs)
+      const twitterTracking = trackingResult.hostNavigationStats[dates.Today]['twitter.com']
+      const twitterApiTracking = trackingResult.hostNavigationStats[dates.Today]['www.api.twitter.com']
+      assert.equal(twitterTracking, 10)
+      assert.equal(twitterApiTracking, 1)
+    })
+    it('should not increment time on website which is not tracked', function () {
+      const tabs = [{url: 'https://example.com/test'}]
+      const trackingResult = this.bgScript.trackWebsite(storage, tabs)
+      const exampleTracking = trackingResult.hostNavigationStats[dates.Today]['example.com']
+      assert.equal(exampleTracking, undefined)
     })
   })
 })
